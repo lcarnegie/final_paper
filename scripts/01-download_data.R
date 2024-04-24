399e0e3174bd54a11c8776cf201b6707eb753c59
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2024-04-24 09:55:24 -0400

    further styled code.

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -23,14 +23,14 @@ raw_data <-
     "https://www.billboard.com/charts/greatest-hot-100-artists/"
   )
 
-write_html(raw_data, 'data/raw_data/greatest100.html')
+write_html(raw_data, "data/raw_data/greatest100.html")
 
-raw_data <- read_html('data/raw_data/greatest100.html')
+raw_data <- read_html("data/raw_data/greatest100.html")
 
-names <- raw_data |> 
-          html_elements("h3") |> 
-          html_text() |> 
-          head(100)
+names <- raw_data |>
+  html_elements("h3") |>
+  html_text() |>
+  head(100)
 
 artists <- trimws(gsub("\\s+", " ", names))
 
@@ -45,20 +45,19 @@ artist_top_tracks_list <- list()
 
 # Loop through each artist and store their data
 for (artist_name in artists) {
-  
   # Get audio features
   audio_features <- get_artist_audio_features(artist_name)
   # Extract artist_id and use it to get top tracks
   artist_id <- audio_features$artist_id[1] # Assuming 'artist_id' is correctly named and not null
-  
+
   # Get top tracks for the US market
   top_tracks <- get_artist_top_tracks(artist_id, market = "US")
-  
+
   # Store data frames in respective lists
   artist_audio_features_list[[artist_name]] <- audio_features
   artist_top_tracks_list[[artist_name]] <- top_tracks
-  
-  Sys.sleep(sample(0:12)) #Stop for a random time to not get kicked out of API
+
+  Sys.sleep(sample(0:12)) # Stop for a random time to not get kicked out of API
   print(paste("Got", artist_name, "'s data"))
 }
 
@@ -75,5 +74,3 @@ top_tracks <- as_tibble(top_tracks_tibble_spotify)
 
 write_csv(audio_features, "data/raw_data/audio_features.csv")
 write_csv(top_tracks, "data/raw_data/top_tracks.csv")
-
-         
--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,6 +1,6 @@
 #### Preamble ####
-# Purpose: Cleans the raw Spotify popularity/audio feature data and combines it 
-#          into a single dataset. 
+# Purpose: Cleans the raw Spotify popularity/audio feature data and combines it
+#          into a single dataset.
 # Author: Luca Carnegie
 # Date: 19 April 2024
 # Contact: luca.carnegie@mail.utoronto.ca
@@ -16,71 +16,72 @@ top_tracks <- read.csv("data/raw_data/top_tracks.csv")
 audio_features <- read.csv("data/raw_data/audio_features.csv")
 
 
-#Clean the two CSVs
-cleaned_top_tracks <- top_tracks |> 
-                      select(Artist_Name, name, popularity) |>
-                      rename(song_name = name) |>
-                      clean_names() |>
-                      mutate(song_id = paste(artist_name, song_name, sep = " - "))
-                      
-                      
-                          
-cleaned_audio_features <- audio_features|>
-                          mutate(explicit_ = if_else(explicit, 1, 0)) |>
-                          select(artist_name, 
-                                 track_name,
-                                 valence,
-                                 danceability,
-                                 explicit_,
-                                 loudness,
-                                 mode, 
-                                 duration_ms
-                                 ) |>
-                          rename(song_name = track_name) |>
-                          clean_names() |>
-                          mutate(song_id = paste(artist_name, song_name, sep = " - "))
-                          
+# Clean the two CSVs
+cleaned_top_tracks <- top_tracks |>
+  select(Artist_Name, name, popularity) |>
+  rename(song_name = name) |>
+  clean_names() |>
+  mutate(song_id = paste(artist_name, song_name, sep = " - "))
 
-#Merge based on song name
+
+
+cleaned_audio_features <- audio_features |>
+  mutate(explicit_ = if_else(explicit, 1, 0)) |>
+  select(
+    artist_name,
+    track_name,
+    valence,
+    danceability,
+    explicit_,
+    loudness,
+    mode,
+    duration_ms
+  ) |>
+  rename(song_name = track_name) |>
+  clean_names() |>
+  mutate(song_id = paste(artist_name, song_name, sep = " - "))
+
+
+# Merge based on song name
 cleaned_ds <- cleaned_audio_features |>
   # Perform an inner join on 'song_name' to filter only relevant songs from audio features
-  inner_join(cleaned_top_tracks, by = "song_id") 
+  inner_join(cleaned_top_tracks, by = "song_id")
 
-# We lose a few songs from each artist in the inner join because of a disconnect 
+# We lose a few songs from each artist in the inner join because of a disconnect
 # between the top_songs dataset and the audio_features dataset - some songs on the
-# top 10 don't seem to have audio features that are query-able. 
+# top 10 don't seem to have audio features that are query-able.
 
-# There were also duplicates, so drop ones with the lower popularity score.  
-# After all, we are interested in what makes a song "more" popular. 
+# There were also duplicates, so drop ones with the lower popularity score.
+# After all, we are interested in what makes a song "more" popular.
 
 cleaned_ds <- cleaned_ds |>
   group_by(song_id) |>
-  slice_max(order_by = popularity, with_ties = FALSE) |> # Remove duplicates 
+  slice_max(order_by = popularity, with_ties = FALSE) |> # Remove duplicates
   ungroup() |>
-  select(-artist_name.y, -song_name.y) |> #drop artifacts from inner join
+  select(-artist_name.y, -song_name.y) |> # drop artifacts from inner join
   clean_names() |>
   mutate(
-    duration_ms = duration_ms/1000,  #convert to seconds
-    explicit  = as.integer(explicit)
+    duration_ms = duration_ms / 1000, # convert to seconds
+    explicit = as.integer(explicit)
   ) |>
   rename(
-    song_name = song_name_x, 
+    song_name = song_name_x,
     artist_name = artist_name_x,
     duration_secs = duration_ms
   ) |>
   select(
-    artist_name, 
+    artist_name,
     song_name,
-    popularity, 
-    mode, 
-    valence, 
-    danceability, 
-    explicit, 
-    loudness, 
+    popularity,
+    mode,
+    valence,
+    danceability,
+    explicit,
+    loudness,
     duration_secs
   )
-  
-  
+
+
 
 # Check the top entries of the merged dataset to ensure correctness
 head(cleaned_ds)
@@ -93,4 +94,3 @@ file_path <- "data/analysis_data/dataset.parquet"
 
 # Write the DataFrame to a Parquet file at the specified file path
 write_parquet(cleaned_ds, file_path)
-
